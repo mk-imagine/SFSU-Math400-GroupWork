c4785be09ac03f71c154be008afd5531e5e46397
ntun@mail.sfsu.edu <ntun@mail.sfsu.edu>
2021-02-10 14:26:21 -0800

    HW_2.5-a-b-c Version2

--- hw2/hw2_400_v2.m
+++ hw2/hw2_400_v2.m
@@ -1,3 +1,10 @@
+
+% Problem 5
+%
+% February 10, 2021
+% Group 2: Mark Kim, Amber Hardigan, Adrian Lopez, Nyan Tun, Alyssa Reyes
+
+
 % plot the ellipse
  x = [-2 : 0.01 : 2];
  y = sqrt(1 - x.^2/4);
@@ -11,17 +18,19 @@
  y = 2*rand - 1;
 % plot the point
  plot(x,y, '.r', -x, -y);
-% if the point is inside the ellipe, increment our count
+% check if the point is inside the ellipe
+% count the number of points inside the ellipse 
  if (x^2 / 4 + y^2 <= 1)
  inside = inside + 1;
  end;
  end;
 
- 
+ %estimate the area inside the ellipse 
  area = inside / 1000 * 8
  
  outside = 1000-inside; 
  
+ %estimate the expected mean and variance of Xi
  mean = inside/1000 * 1 + outside/1000 * 0; 
  expectedArea = inside/1000 * 1^2 + outside/1000 * 0^2;
  variance = expectedArea - mean^2;
