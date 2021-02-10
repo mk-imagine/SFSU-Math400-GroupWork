d7cabd4c55b8053715c58cf90bdc5aad9f0239e9
mkim797 <39310405+mkim797@users.noreply.github.com>
2021-02-10 14:33:01 -0800

    added montecarlotest

---
+++ hw2/montecarlotest.m
@@ -0,0 +1,13 @@
+mult = 100;         % multiplierev
+
+nexpts = 4;         % number of experiments
+npts = 100;         % number of points generated (initial)
+ev = zeros(4,3);    % approximation to integral and standard deviation
+
+for i=1:nexpts
+    x=rand(npts,2); y=rand(npts,1);         % generate points in [0,2] x [0,1]
+    if (x < 2 && y < 1)
+        inside = inside + 1;
+    else
+        outside = outside + 1;
+end
