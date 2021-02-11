2cc8136642edb480782bf30e8eeadfa31ad64ec0
mkim797 <39310405+mkim797@users.noreply.github.com>
2021-02-10 16:08:42 -0800

    changed graph to polar on hw_400_v2.m

--- hw2/montecarlotest.m
+++ hw2/montecarlotest.m
@@ -6,7 +6,7 @@ ev = zeros(4,3);    % approximation to integral and standard deviation
 
 for i=1:nexpts
     x=rand(npts,2); y=rand(npts,1);         % generate points in [0,2] x [0,1]
-    if (x < 2 && y < 1)
+    if (x^2 / 4 + y^2 <= 1)
         inside = inside + 1;
     else
         outside = outside + 1;
