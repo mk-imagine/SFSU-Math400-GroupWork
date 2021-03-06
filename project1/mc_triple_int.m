a4cacbc62431224b3678d040fd95f4a99e8067c4
mkim797 <39310405+mkim797@users.noreply.github.com>
2021-03-05 19:53:26 -0800

    code cleanup and readability

--- project1/mc_triple_int.m
+++ project1/mc_triple_int.m
@@ -10,7 +10,7 @@ fval = zeros(N,1);
 for i=1:N               % Loop over sample points
     % Generate a point in the region (parallelepiped)
     x = 2*rand - 1; y = 2*rand - 1; z = 2*rand - 1;
-    if x^2+y^2+z^2 <= 1 % Check if integration region
+    if x^2 + y^2 + z^2 <= 1 % Check if integration region
         temp = f(x,y,z);
         fval(i) = temp;
         int = int + temp;
