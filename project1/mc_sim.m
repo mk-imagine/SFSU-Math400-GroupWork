ad6270fbfc294b2f8b47d298a0d81ed4ae5827db
mkim797 <mkim797@yahoo.com>
2021-03-07 12:50:07 -0800

    minor changes to mc_sim.m: variables renamed and comments clarified

--- project1/mc_sim.m
+++ project1/mc_sim.m
@@ -12,23 +12,23 @@ zero_wait = 0;                  % number of times there was zero wait
 % Simulate Queue at Box Office
 for i=1:numsims
     % Initialize Variables for each Simulation
-    wait = 0;    % number of people who have to wait
-    change = 0;  % number of people needing change
-    fives = 0;   % number of fives in register
+    wait = 0;       % number of people who had to wait
+    need_change = 0;% number of people needing change
+    fives = 0;      % number of fives in register
     for j=1:numppl
         prob = rand;
         % if person does not need change (has a five)
         if prob <= 0.5
-            if change == 0          % if no one needs change
+            if need_change == 0     % if no one needs change
                 fives = fives + 1;  % add to fives bucket
             else                    % else if someone needs change
-                change = change - 1;% reduce number of people that need change (no fives go in bucket)
+                need_change = need_change - 1;% reduce number of people that need change (no fives go in bucket)
             end
         % else if person needs change (has a ten)
         else
             if fives == 0           % if fives bucket empty
                 wait = wait + 1;    % person has to wait
-                change = change + 1;% and add to number of people waiting for change
+                need_change = need_change + 1;% and add to number of people waiting for change
             else                    % else if change is available
                 fives = fives - 1;  % give change (no wait)
             end
